f3ace500d7e88046a99548dc06cbd913a7ae3063
Luís <90935726+ElanTk@users.noreply.github.com>
2025-07-29 14:43:04 -0300

    Add files via upload
    Código inicial que testa a linguagem.

---
+++ testeFowa/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Ovo me mata");
+        int x = 10;
+        int y = 20;
+        int z = x * y;
+
+        System.out.println("Z deu " + z + " my man.");
+    }
+
+}
